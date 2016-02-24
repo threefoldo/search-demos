14fa3564a93293cdd74300dd4ef2f6db72a80883
threefoldo <threefoldo@gmail.com>
2016-02-24 14:15:29 +0800

    load sample data

---
+++ instant-search/server/load.js
@@ -0,0 +1,19 @@
+Movies = new Meteor.Collection('Movies');
+
+var process_data = function(item) {
+    item.credit = item.credit.replace(/\n/g, '').trim();
+    item.genre = item.genre.replace(/\s/g, '').split('|');
+    item.yeartype = item.yeartype.replace(/\(|\)/g, '');
+    // console.log(item);
+    return item;
+};
+
+// load sample data
+Meteor.startup(function() {
+    // Movies.remove({});
+    if (Movies.find({}).count() < 10) {
+        JSON.parse(Assets.getText('topmovies.json')).forEach(function(item) {
+            Movies.insert(process_data(item));
+        });
+    }
+});
