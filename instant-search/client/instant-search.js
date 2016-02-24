4ea83f02aff761d240674bcd768590460920f4b2
threefoldo <threefoldo@gmail.com>
2016-02-24 16:59:39 +0800

    add mobile-menu

--- instant-search/client/instant-search.js
+++ instant-search/client/instant-search.js
@@ -1,25 +1,45 @@
-Movies = Meteor.Collection();
+Movies = new Meteor.Collection('Movies');
 
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+Template.content.helpers({
+    movies: function() {
+        var genre = Session.get('genre') || '';
+        var keyword = Session.get('search');
+        return Movies.find({});
     }
-  });
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+Template.menu.helpers({
+    genres: function() {
+        // return Movies.find({}, {fields: {yeartype: true}});
+        return _.uniq(_.flatten(Movies.find({}, {
+            fields: {genre: true}
+        }).fetch().map(function(x) {
+            return x.genre;
+        })).sort(), true);
     }
-  });
+});
+
+Template.content.rendered = function() {
+    $('#mmenu').mmenu({
+        classes: "mm-white",
+        header: true,
+        buttonbar: "Buttonbar",
+        counters: true,
+        offCanvas: {
+            position: "left",
+            zposition: "front"
+        },
+        onClick: {
+            blockUI: false,
+            close: true,
+            preventDefault: false,
+            setSelected: true
+        }
+    });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.content.events({
+    'click .genre': function (e) {
+
+    }
+});
